322fcd13cc0b68d03c59da39e5923a4ba32b8694
cherrynuna <inyoungstar7@gmail.com>
2026-03-02 00:58:38 +0900

    first commit

---
+++ pp0/ex00/Hello.py
@@ -0,0 +1,11 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "S. Korea!")
+ft_set = {"Hello", "Seoul!"}
+ft_dict = {"Hello" : "42Seoul!"}
+
+#your code here
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
