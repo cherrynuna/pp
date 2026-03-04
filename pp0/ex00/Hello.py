9b6282248bc0e114b28a262512b40e44d5b51860
cherrynuna <inyoungstar7@gmail.com>
2026-03-05 00:30:22 +0900

    p0-00

--- pp0/ex00/Hello.py
+++ pp0/ex00/Hello.py
@@ -1,10 +1,22 @@
-ft_list = ["Hello", "World!"]
-ft_tuple = ("Hello", "S. Korea!")
-#ft_set = {"Hello", "Seoul!"}
-ft_set = "{'Hello', 'Seoul!'}"
-ft_dict = {"Hello" : "42Seoul!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
 
 #your code here
+
+ft_list[1] = "World!"
+
+# A tuple is a collection which is ordered and unchangeable.
+# Since tuples are immutable, we need to convert it to a list first to make changes.
+tmp_lst = list(ft_tuple)
+tmp_lst[1] = "S. Korea!"
+ft_tuple = tuple(tmp_lst)
+
+ft_set.remove("tutu!")
+ft_set.add("Seoul!")
+
+ft_dict["Hello"] = "42Seoul!"
 
 print(ft_list)
 print(ft_tuple)
