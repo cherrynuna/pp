aab9eaf1ee1923bfbc8fa0f218dc287427cd036e
cherrynuna <inyoungstar7@gmail.com>
2026-03-04 15:53:09 +0900

    p0-02

--- pp0/ex01/format_ft_time.py
+++ pp0/ex01/format_ft_time.py
@@ -2,9 +2,6 @@ import time
 
 current_time = time.time()
 
-print("Seconds since January 1, 1970: {:,.4f} or {:.2e} in scientific notation".format(
-    current_time,
-    current_time
-))
+print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation")
 
 print(time.strftime("%b %d %Y"))
---
+++ pp0/ex02/find_ft_type.py
@@ -0,0 +1,5 @@
+def all_thing_is_obj(object: any) -> int:
+
+    print(type(object).__name__)
+
+    return 42
