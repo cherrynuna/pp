613148475007d46558e9b1f0066d674efe32e7f2
cherrynuna <inyoungstar7@gmail.com>
2026-03-04 23:49:36 +0900

    p0-04

---
+++ pp0/ex04/whatis.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
+        print("AssertionError:  more  than  one  argument  is  provided")
+    else:
+        try:
+            n = int(sys.argv[1])
+            if n % 2 == 0:
+                print("I'm  Even.")
+            else:
+                print("I'm  Odd.")
+        except ValueError:
+            print("AssertionError:  argument  is  not  an  integer")
