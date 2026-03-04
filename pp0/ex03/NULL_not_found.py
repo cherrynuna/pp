09d92b90bec4b8a22964ac3e8223a750ae242d53
cherrynuna <inyoungstar7@gmail.com>
2026-03-05 00:43:43 +0900

    p0-03 fixed

--- pp0/ex03/NULL_not_found.py
+++ pp0/ex03/NULL_not_found.py
@@ -1,14 +1,14 @@
 def NULL_not_found(object: any) -> int:
     if object is None:
-        print(f"Nothing: {object} {type(object)}")
+        print("Nothing:", object, type(object))
     elif isinstance(object, float) and object != object:
-        print(f"Cheese: {object} {type(object)}")
+        print("Cheese:", object, type(object))
     elif object is False:
-        print(f"Fake: {object} {type(object)}")
+        print("Fake:", object, type(object))
     elif object == 0 and isinstance(object, int):
-        print(f"Zero: {object} {type(object)}")
+        print("Zero:", object, type(object))
     elif object == "" and isinstance(object, str):
-        print(f"Empty: {type(object)}")
+        print("Empty:", type(object))
     else:
         print("Type not Found")
         return 1
