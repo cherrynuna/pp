52a63838133f68ee84f38102297453f3e20b8161
cherrynuna <inyoungstar7@gmail.com>
2026-07-08 02:14:55 +0900

    fix: i transferred to 42paris

--- pp0/ex00/Hello.py
+++ pp0/ex00/Hello.py
@@ -8,12 +8,12 @@ ft_dict = {"Hello" : "titi!"}
 ft_list[1] = "World!"
 
 # A tuple is a collection which is ordered and unchangeable.
-ft_tuple = ("Hello", "S. Korea!")
+ft_tuple = ("Hello", "France!")
 
 ft_set.remove("tutu!")
-ft_set.add("Seoul!")
+ft_set.add("Paris!")
 
-ft_dict["Hello"] = "42Seoul!"
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
